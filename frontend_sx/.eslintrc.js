33620fdc93fdfa1bbe25a9ff7af3d369befffcc1
davorNikoloski <nikoloski.davorr@gmail.com>
2023-08-24 11:28:43 +0200

    s

---
+++ frontend_sx/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: "babel-eslint", // or "@typescript-eslint/parser" for TypeScript
+    extends: ["eslint:recommended", "plugin:react/recommended"], // Adjust as needed
+    plugins: ["react"],
+    rules: {
+      // Add your custom rules here
+    },
+  };
+  
