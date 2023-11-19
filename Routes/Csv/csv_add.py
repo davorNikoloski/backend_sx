c5b7afc6fd8fcebe108bd9666f827ca548df5c13
davorNikoloski <nikoloski.davorr@gmail.com>
2023-11-19 13:12:16 +0100

    csv add

--- Routes/Csv/csv_add.py
+++ Routes/Csv/csv_add.py
@@ -87,6 +87,8 @@ def upload_csv_subcategory():
 
 
 
+
+
 @csv_api.route('/product_csv', methods=['POST'])
 def upload_csv_product():
     try:
@@ -110,7 +112,6 @@ def upload_csv_product():
                 product_name = row['name']
                 product_info = row['info']
                 product_price = row['price']
-                product_price_Discount = row['price_Discount']
                 product_productNo = row['productNo']
                 product_description = row.get('description', '')  # Handle optional field
                 product_brand = row.get('brand', '')  # Handle optional field
@@ -119,11 +120,11 @@ def upload_csv_product():
                 product_scid = row['scid']  # Assuming 'scid' is provided in the CSV
 
                 # Check for and handle image paths in the CSV
-                product_gif_path = row.get('product_path')
-                product_product_paths = row.get('product_paths').split(',')  # Assuming multiple paths are comma-separated
+                gif_paths = row.get('product_path')
+                product_paths = row.get('product_paths').split(',')  # Assuming multiple paths are comma-separated
 
                 if 'product_images[]' in request.files:
-                    product_images = request.files.getlist('product_images[]')
+                   # product_images = request.files.getlist('product_images[]')
                     print(product_images[0])
                     for product_image in product_images:
                         print("Uploaded file name:", product_image.filename)
@@ -141,7 +142,7 @@ def upload_csv_product():
 
 
                 if 'product_gifs[]' in request.files:
-                    product_gifs = request.files.getlist('product_gifs[]')
+                   # product_gifs = request.files.getlist('product_gifs[]')
                     print(product_gifs[0])
                     for product_gif in product_gifs:
                         print("Uploaded file name:", product_gif.filename)
@@ -157,8 +158,11 @@ def upload_csv_product():
                         else:
                             return custom_abort(400, "Invalid file format. Allowed formats: jpg, jpeg, png, gif")
 
-                product_paths_str = ','.join(product_product_paths)
-                #gif_paths = ','.join(product_gifs_paths)
+
+
+
+                product_paths_str = ','.join(product_paths)
+                gif_paths = ','.join(product_gifs_paths)
 
 
                 # Query the 'Categories' and 'Subcategories' tables to find the associated category and subcategory
@@ -171,26 +175,22 @@ def upload_csv_product():
                         name=product_name,
                         info=product_info,
                         price=product_price,
-                        price_Discount = product_price_Discount,
                         productNo=product_productNo,
                         description=product_description,
                         brand=product_brand,
                         color=product_color,
                         cid=category.cid,
                         scid=subcategory.scid,
-                        product_path=product_gif_path,
+                        product_path=gif_filename,
                         product_paths=product_paths_str
                     )
-                    print (product_gif_path + 'product_gif_path')
-                    for pr_p in product_paths_str:
-
-                        print (pr_p + 'pr_p')
+                    print (product_paths)
                     db.session.add(new_product)
                 else:
                     return f"Error: Category with cid={product_cid} or Subcategory with scid={product_scid} not found in the database."
-            print('------ ADDED -----' +product_name)
+
             db.session.commit()
-            
+
             return "CSV data uploaded and processed successfully."
 
         return "No file selected or an error occurred."
